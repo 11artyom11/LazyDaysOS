5ef21398893387d1e5a65355044533ee12ca9e91
Artyom Grigorian <grigorianartyom1@gmail.com>
2024-01-07 21:15:36 +0400

    Fixed a bug with interrupts and segmentation

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,6 +7,7 @@ void kernel_main(void)
 	/* Initialize terminal interface */
 	terminal_initialize();
 	init_idt();
+	asm("int $4");	// Don't remove this line, it's critical to make interrupts work, idonno why LLOL
 	k_print("[INFO] Setting up kernel...\n");
 	asm("push %eax \n\
 		 mov %cr0, %eax");
@@ -18,7 +19,6 @@ void kernel_main(void)
 		k_print("[INFO] We're now in real mode cr0: %d \n", cr0);
 	}
 
-	asm("int $4");	
 	boot_successful = true;
 	if (boot_successful){
 		k_print("[SUCC] Kernel is ready\n");
