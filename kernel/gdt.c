780d02f96a32f296bb4fa9f0f409c3de82f75db5
Artyom Grigorian <grigorianartyom1@gmail.com>
2024-06-16 11:24:09 +0400

    Fixed typo

--- kernel/gdt.c
+++ kernel/gdt.c
@@ -51,18 +51,18 @@ typedef enum
 } GDT_FLAGS;
 
 // Helper macros
-#define GDTlb$limit_LOW(limit)                (limit & 0xFFFF)
+#define GDT_LIMIT_LOW(limit)                (limit & 0xFFFF)
 #define GDT_BASE_LOW(base)                  (base & 0xFFFF)
 #define GDT_BASE_MIDDLE(base)               ((base >> 16) & 0xFF)
-#define GDT_FLAGSlb$limit_HI(limit, flags)    (((limit >> 16) & 0xF) | (flags & 0xF0))
+#define GDT_FLAGS_LIMIT_HI(limit, flags)    (((limit >> 16) & 0xF) | (flags & 0xF0))
 #define GDT_BASE_HIGH(base)                 ((base >> 24) & 0xFF)
 
 #define GDT_ENTRY(base, limit, access, flags) {                     \
-    GDTlb$limit_LOW(limit),                                           \
+    GDT_LIMIT_LOW(limit),                                           \
     GDT_BASE_LOW(base),                                             \
     GDT_BASE_MIDDLE(base),                                          \
     access,                                                         \
-    GDT_FLAGSlb$limit_HI(limit, flags),                               \
+    GDT_FLAGS_LIMIT_HI(limit, flags),                               \
     GDT_BASE_HIGH(base)                                             \
 }
 
@@ -72,13 +72,13 @@ static GDTEntry g_GDT[] = {
 
     // Kernel 32-bit code segment
     GDT_ENTRY(0,
-              0xFFFFF,
+              0x7FFFF,
               GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_CODE_SEGMENT | GDT_ACCESS_CODE_READABLE,
               GDT_FLAG_32BIT | GDT_FLAG_GRANULARITY_4K),
 
     // Kernel 32-bit data segment
     GDT_ENTRY(0,
-              0xFFFFF,
+              0x80000,
               GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_DATA_SEGMENT | GDT_ACCESS_DATA_WRITEABLE,
               GDT_FLAG_32BIT | GDT_FLAG_GRANULARITY_4K),
 
