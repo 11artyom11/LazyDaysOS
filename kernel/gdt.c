0b61ad6f3079e6e03daafdbafca5b152065217b9
Artyom Grigorian <grigorianartyom1@gmail.com>
2024-10-27 13:53:49 +0400

    Fixed macro naming issue

--- kernel/gdt.c
+++ kernel/gdt.c
@@ -51,18 +51,18 @@ typedef enum
 } GDT_FLAGS;
 
 // Helper macros
-#define GDTlb$limit_LOW(limit)                (limit & 0xFFFF)
+#define GDT_LIMIT_LOW(limit)                (limit & 0xFFFF)
 #define GDT_BASE_LOW(base)                  (base & 0xFFFF)
 #define GDT_BASE_MIDDLE(base)               ((base >> 16) & 0xFF)
-#define GDT_FLAGSlb$limit_HI(limit, flags)    (((limit >> 16) & 0xF) | (flags & 0xF0))
+#define GDT_FLAGS_LIMIT_HI(limit, flags)    (((limit >> 16) & 0xF) | (flags & 0xF0))
 #define GDT_BASE_HIGH(base)                 ((base >> 24) & 0xFF)
 
 #define GDT_ENTRY(base, limit, access, flags) {                     \
-    GDTlb$limit_LOW(limit),                                           \
+    GDT_LIMIT_LOW(limit),                                           \
     GDT_BASE_LOW(base),                                             \
     GDT_BASE_MIDDLE(base),                                          \
     access,                                                         \
-    GDT_FLAGSlb$limit_HI(limit, flags),                               \
+    GDT_FLAGS_LIMIT_HI(limit, flags),                               \
     GDT_BASE_HIGH(base)                                             \
 }
 
